4c7e052319098033938ac9097c72b77af9d913ae
omarXzain <omaren2010@gmail.com>
2020-12-04 19:42:10 +0200

    added test to binary

--- tests/test_array_binary_search.py
+++ tests/test_array_binary_search.py
@@ -3,6 +3,6 @@ from data_structures_and_algorithms.challenges.array_binary_search.array_binary_
 def test_BinarySearch():
     arr = [ 25, 35, 65, 80, 85,86,96,97,98,102,112 ] 
     target = 96
-    actual = BinarySearch(arr, target)
+    actual = BinarySearch( arr, target )
     expected = 6
     assert actual == expected
