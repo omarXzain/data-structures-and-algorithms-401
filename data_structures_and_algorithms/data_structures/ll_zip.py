5923e791c7a059e26358ba1afc15182bd387a1e8
omarXzain <omaren2010@gmail.com>
2020-12-11 19:07:56 +0200

    zip fixed

---
+++ data_structures_and_algorithms/data_structures/ll_zip.py
@@ -0,0 +1,27 @@
+from data_structures_and_algorithms.data_structures.linked_list.linked_list import Linkedlist
+
+
+def zip_lists(list1, list2):
+    finalList = Linkedlist()
+    current1 = list1.head
+    current2 = list2.head
+    while current1 and current2:
+        finalList.append(current1.value)
+        finalList.append(current2.value)
+        current1 = current1.next
+        current2 = current2.next
+    return finalList
+
+if __name__ == "__main__":
+    li=Linkedlist()
+    li2=Linkedlist()
+    li.append(1)
+    li.append(2)
+    li.append(3)
+    li2.append(9)
+    li2.append(10)
+    li2.append(11)
+    print(li.to_string())
+    print(li2.to_string())
+    # print('second: ',l2.to_string())
+    print('final: ',zip_lists(li,li2).to_string())
